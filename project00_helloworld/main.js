a75bb191a37471c74965d0fc928b83c45b3f3009
smahsan053 <smahsan053@gmail.com>
2024-02-20 19:10:42 +0500

    first commit

---
+++ project00_helloworld/main.js
@@ -0,0 +1,3 @@
+"use strict";
+let message = 'Thank You Governor Sindh "Kamran Khan Tessori"';
+console.log(message);
---
+++ project00_helloworld/main.ts
@@ -0,0 +1,2 @@
+let message = 'Thank You Governor Sindh "Kamran Khan Tessori"'
+console.log(message);
