758b9d815cc7a85832da8d15050d5956dea7b947
smahsan053 <smahsan053@gmail.com>
2024-02-27 22:27:42 +0500

    sixth commit

---
+++ Learning Projects/project01_variables/main.ts
@@ -0,0 +1,76 @@
+// Variable declaration
+// Inference Type
+
+// String
+let myName = "Ahsan"
+console.log(myName);
+myName = "Hussain"
+console.log(myName);
+// myName = 33 // Not Possible
+// console.log(myName);
+
+// Number
+let myNum = 33
+console.log(myNum);
+myNum = 44
+console.log(myNum);
+// myNum = "33" // Not Possible
+// console.log(myNum);
+
+// Annotation Type
+
+// String
+let newName: string = "Ahsan"
+console.log(newName);
+// let newName:string = "Hussain" // Not Possible
+// console.log(newName);
+// newName:string = "Hussain" // Not Possible
+// console.log(newName);
+newName = "Hussain"
+console.log(newName);
+// let newName:number = 22 // Not Possible
+// console.log(newName);
+// newName:number = 22 // Not Possible
+// console.log(newName);
+// newName = 22 // Not Possible
+// console.log(newName);
+
+// Number
+let luckyNum: number = 22;
+console.log(luckyNum);
+// let luckyNum: number = 22; // Not Possible
+// console.log(luckyNum);
+// luckyNum: number = 22; // Not Possible
+// console.log(luckyNum);
+luckyNum = 22;
+console.log(luckyNum);
+// let luckyNum: string = "22"; // Not Possible
+// console.log(luckyNum);
+// luckyNum: string = "22"; // Not Possible
+// console.log(luckyNum);
+// luckyNum = "22"; // Not Possible
+// console.log(luckyNum);
+
+// Any
+let numChar: any = "Ahsan"
+console.log(numChar);
+// let numChar:any = "Hussain" // Not Possible
+// console.log(numChar);
+// numChar: any = "Hussain" // Not Possible
+// console.log(numChar);
+numChar = "Hussain"
+console.log(numChar);
+numChar = 44
+console.log(numChar);
+
+// Boolean
+let isLoggedIn: boolean = true
+console.log(isLoggedIn);
+isLoggedIn = false
+console.log(isLoggedIn);
+
+
+
+
+
+
