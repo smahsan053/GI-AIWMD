73e3b712eebe5ce15d45a5d0c056b5ab8ff7ea6c
smahsan053 <smahsan053@gmail.com>
2024-03-16 13:16:41 +0500

    simple cli calculator

---
+++ Assignments/Assignment # 02 - Simple CLI Calculator/index.ts
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    { message: "Enter First Number", type: 'number', name: "firstNumber" },
+    { message: "Enter Second Number", type: 'number', name: "secondNumber" },
+    {
+        message: "Enter Operator",
+        type: "list",
+        name: "operator",
+        choices: ["+", "-", "*", "/"],
+    },
+]);
+// console.log(answer);
+if (answer.operator === "+") {
+    console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "-") {
+    console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "*") {
+    console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "/") {
+    console.log(answer.firstNumber / answer.secondNumber);
+} else {
+    console.log("Please enter a valid operator");
+}
